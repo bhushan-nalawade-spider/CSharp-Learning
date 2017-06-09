a7db5eb3cdbb9d7eefc12d7cbaccf2137dc1ecdb
Mayuresh Ahirrao <mahirrao@spiderlogic.com>
2017-06-09 17:06:30 +0530

    First Commit

---
+++ Mayuresh/PracticePrograms/PracticePrograms/LargestSmallest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticePrograms
+{
+    public class LargestSmallest
+    {
+        public static void Main(string[] args)
+        {
+            const int arraySize = 10;
+
+            /*int[] numberArray = {5, 46, 986456, 56, 897,
+                                    5351, 43, 987, 77554, 2 };   */
+
+            int[] numberArray = new int[arraySize];
+
+            for (int number=0; number < arraySize; number++ )
+            {
+                Console.WriteLine("Enter number " +(number+1) +" :");
+                string str = Console.ReadLine();
+
+                int enteredNum = Convert.ToInt32(str);
+
+/*                while (enteredNum == 0)
+                {
+                    Console.WriteLine(" The number " + enteredNum + " is not allowed. Enter again:");
+
+                    str = Console.ReadLine();
+                    enteredNum = Convert.ToInt32(str);
+                }   */
+
+                numberArray[number] = enteredNum;
+            }
+
+            int largestInt = numberArray [0];
+            int smallestInt =numberArray [0];
+            foreach (int number in numberArray)
+            {
+                if (largestInt < number)
+                    largestInt= number;
+                if (smallestInt > number)
+                    smallestInt = number;
+            }
+
+            Console.WriteLine ("Largest number is: " + largestInt);
+            Console.WriteLine ("Smallest number is: " + smallestInt);
+
+        }
+    }
+}
