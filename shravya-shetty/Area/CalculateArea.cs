db408f50b6bc9028b0af595af88c134440db087c
shravya-shetty-spider <sshetty@spiderlogic.com>
2017-06-22 11:57:47 +0530

    Area Program

---
+++ shravya-shetty/Area/CalculateArea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    class CalculateArea : Area, ICalculateArea
+    {
+        public int getArea()
+        {
+            return (width * height);
+        }
+        public int getCost(int area)
+        {
+            return area * 100;
+        }
+    }
+}
+
+
+      
---
+++ shravya-shetty/Area/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    class Area
+    {
+        public int width;
+        public int height;
+        public void setWidth(int w)
+        {
+            width = w;
+        }
+        public void setHeight(int h)
+        {
+            height = h;
+        }
+
+
+        static void Main(string[] args)
+        {
+            CalculateArea ca = new CalculateArea();
+            int area;
+            ca.setWidth(5);
+            ca.setHeight(7);
+            area = ca.getArea();
+
+            // Print the area of the object.
+            Console.WriteLine("Total area: {0}", ca.getArea());
+            Console.WriteLine("Total paint cost: ${0}", ca.getCost(area));
+            Console.ReadKey();
+        }
+    }
+}
+        
+
