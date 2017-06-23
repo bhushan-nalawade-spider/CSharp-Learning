c2eef517765c4354b6b2aec5d3467eed15489352
shravya-shetty-spider <sshetty@spiderlogic.com>
2017-06-23 15:18:29 +0530

    Two files

---
+++ shravya-shetty/OnAndOff/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace OnAndOff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, j = 0;
+            DateTime[] offDates = new DateTime[10];
+            Console.WriteLine("Enter a Range from date: ");
+            DateTime fromRange = readDate();
+            Console.WriteLine("Enter a Range To date: ");
+            DateTime toRange = readDate();
+            while (i == 1)
+            {
+                Console.WriteLine("Enter the off dates");
+                offDates[j++] = readDate();
+                Console.WriteLine("press 1 to continue 0 to exit");
+                i = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+
+            for (int k = 0; k < offDates.Length; k++)
+            {
+                Console.WriteLine("off dates" + offDates[k]);
+                Console.WriteLine("On dates from" + offDates[k] + " to " + offDates[k + 1].AddDays(-1));
+                Console.ReadLine();
+            }
+
+
+        }
+
+        private static DateTime readDate()
+        {
+            DateTime userDateTime;
+
+            if (DateTime.TryParse(Console.ReadLine(), out userDateTime))
+            {
+                Console.WriteLine("The day of the week is: " + userDateTime.DayOfWeek);
+
+            }
+            else
+            {
+                Console.WriteLine("You have entered an incorrect value.");
+                System.Environment.Exit(-1);
+            }
+            return userDateTime;
+        }
+    }
+}
+    
+
+
+
+
+
+
---
+++ shravya-shetty/OverlappingNum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+namespace OverlappingNum
+{
+    class OverlappingNum
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] arr2 = { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            int[] intersect = arr1.Intersect(arr2).ToArray();
+            for (int l = 0; l < intersect.Length; l++)
+            Console.WriteLine(intersect[l]);
+            Console.ReadLine();
+        }
+    }
+}
