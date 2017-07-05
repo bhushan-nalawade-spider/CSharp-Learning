afbf0cdc2112429c0e1d96f00d8dd8c7fdbbde3b
Mayuresh Ahirrao <mahirrao@spiderlogic.com>
2017-07-05 17:25:59 +0530

    Programs 6 and 7 completed

---
+++ Mayuresh/PracticePrograms/Pr6_Substrings/Pr6_Substrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr6_Substrings
+{
+    class Pr6_Substrings
+    {
+        static void Main(string[] args)
+        {
+            String sampleString = "abcde";
+
+            Console.WriteLine("The string is: " + sampleString);
+            for(int i = 0; i<sampleString.Length; i++)
+            {
+                String subString = sampleString.Substring(i);
+                for (int j = 1; j <= subString.Length; j++)
+                {
+                    Console.WriteLine(sampleString.Substring(i,j));
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Mayuresh/PracticePrograms/Pr7_Deletefile/Pr7_Deletefile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Pr7_Deletefile
+{
+    class Pr7_Deletefile
+    {
+        static void Main(string[] args)
+        {
+            String fileDirectory = Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString();
+            String fileName = "\\Test.txt";
+            String requiredPath = fileDirectory + fileName;
+
+            //createFile(requiredPath);
+            deleteFile(requiredPath);
+
+            Console.Read();
+        }
+
+        private static void createFile(string requiredPath)
+        {
+            FileStream F = new FileStream(requiredPath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            Console.WriteLine("The file is created at: " + requiredPath);
+        }
+
+        private static void deleteFile(string requiredPath)
+        {
+            if (File.Exists(requiredPath))
+            {
+                File.Delete(requiredPath);
+                Console.WriteLine("File Deleted at " + requiredPath);
+            }
+            else
+                Console.WriteLine("The file does not exist at: " + requiredPath);
+        }
+    }
+}
