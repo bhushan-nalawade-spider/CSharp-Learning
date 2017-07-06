acf81fcd5b538a9d9d1017042cdfbca8b9182a67
Mayuresh Ahirrao <mahirrao@spiderlogic.com>
2017-07-06 10:21:22 +0530

    Program 8 completed

---
+++ Mayuresh/PracticePrograms/Pr8_DuplicateElementsInArray/Pr8_DuplicateElementsInArray.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr8_DuplicateElementsInArray
+{
+    class Pr8_DuplicateElementsInArray
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = 0;
+
+            //Console.WriteLine("Enter the size of the array: ");
+            //arraySize = Convert.ToInt32(Console.ReadLine());
+
+            // int[] sampleArray = createArray(arraySize);
+
+            int[] sampleArray = { 1, 2, 3, 4, 1, 2, 5, 6, 7, 3, 1, 2, 1};
+
+            int numberOfduplicateElements = countDuplicateElementsInArray(sampleArray);
+
+            Console.WriteLine("The number of duplicate elements is: " + numberOfduplicateElements);
+            Console.ReadLine();
+        }
+
+        private static int countDuplicateElementsInArray(int[] sampleArray)
+        {
+            int numberOfduplicateElements = 0;
+
+            
+            for (int i = 0; i < sampleArray.Length; i++ )
+            {
+                int T3 = 0;
+                for (int j = i+1; j < sampleArray.Length; j++)
+                {
+                    int T1 = sampleArray[i];
+                    int T2 = sampleArray[j];
+                    
+                    if (T3 != T2)
+                    {
+                        if (T1 == T2)
+                        {
+                            T3 = T2;
+                            numberOfduplicateElements++;
+                        }
+                    }
+                }
+
+                Console.WriteLine("Loop1");
+            }
+
+            return numberOfduplicateElements;
+        }
+
+        private static int[] createArray(int arraySize)
+        {
+            int[] numberArray = new int[arraySize];
+
+            for (int number = 0; number < arraySize; number++)
+            {
+                Console.WriteLine("Enter number " + (number + 1) + " :");
+                string str = Console.ReadLine();
+
+                int enteredNum = Convert.ToInt32(str);
+
+                /*                while (enteredNum == 0)
+                                {
+                                    Console.WriteLine(" The number " + enteredNum + " is not allowed. Enter again:");
+
+                                    str = Console.ReadLine();
+                                    enteredNum = Convert.ToInt32(str);
+                                }   */
+
+                numberArray[number] = enteredNum;
+            }
+
+            return numberArray;
+        }
+    }
+}
