59f1de93243773f23aa3ed4228477bd916f8b982
Anil pal <pala@spider-india.local>
2017-07-07 17:02:10 +0530

    Common Excercise in JAVA

---
+++ Anil Pal/Java Learnings/src/Substrings.java
@@ -0,0 +1,15 @@
+
+public class Substrings {
+
+	
+	public void findSubstring()
+	{
+		String str = new String("abcde");
+		int charCounter =0;
+		int strlen = str.length();
+		
+		for ( int cnt=0; cnt <str.length(); cnt++)
+			
+	}
+	
+}
