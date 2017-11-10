3debf61636cdfaabb4567e013813ee48d39b37c9
sudharsan-raju-spider <sudharsan-raju-spider>
2017-11-10 16:41:12 +0530

    CSharplearning

---
+++ sudharsan-raju/BinaryTraingle/BinaryTraingle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTriangle();
+        }
+
+        public static void BinaryTriangle()
+        {
+            Console.WriteLine("Enter the number for the traingle to be printed:");
+            string str = Console.ReadLine();
+
+            int maxTriangleLimit = Convert.ToInt32(str);
+
+            for (int i = 0; i < maxTriangleLimit; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(j % 2);
+                }
+                Console.WriteLine(" ");
+            }
+
+            Console.Read();
+        }
+
+    }
+}
---
+++ sudharsan-raju/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortEvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int i, j = 0, k = 0;
+
+
+            Console.Write("Input the comma seperated numbers:");
+            string value = Console.ReadLine();
+            string[] arr = value.Split(',');
+
+            int[] evenarray = new int[arr.Length];
+            int[] oddarray = new int[arr.Length];
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                int number = Int32.Parse(arr[i]);
+                //int number;
+               // if (Int32.TryParse(arr[i], out number))
+                //{
+                    if (number % 2 == 0)
+                    {
+                        evenarray[j] = number;
+                        j++;
+                    }
+                    else
+                    {
+                        oddarray[k] = number;
+                        k++;
+                    }
+
+                }
+
+                Console.Write("\nThe Even elements are : \n");
+                for (i = 0; i < j; i++)
+                {
+                    Console.Write("{0} ", evenarray[i]);
+                }
+
+                Console.Write("\nThe Odd elements are :\n");
+                for (i = 0; i < k; i++)
+                {
+                    Console.Write("{0} ", oddarray[i]);
+                }
+                Console.Write("\n\n");
+                Console.ReadLine();
+            }
+        }
+    }
+
+
+
+        
